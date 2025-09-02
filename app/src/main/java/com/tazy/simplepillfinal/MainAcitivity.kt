69adcd9ea7f8382d65bc9aab5ccca43d943af035
User <luiz_felipe_2002@yahoo.com>
2025-09-02 10:41:52 -0300

    Revert "Primeiros Vinculo"
    This reverts commit 03e111104279420cdb9c55af02b0792a270f31bb.

--- app/src/main/java/com/tazy/simplepillfinal/MainAcitivity.kt
+++ app/src/main/java/com/tazy/simplepillfinal/MainAcitivity.kt
@@ -1,3 +1,4 @@
+// MainActivity.kt
 // F_ARQUIVO: MainActivity.kt
 package com.tazy.simplepillfinal
 
@@ -5,20 +6,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.rememberNavController // <-- IMPORTANTE: Adicione esta linha
 import com.tazy.simplepillfinal.navigation.NavGraph
-import com.tazy.simplepillfinal.ui.theme.SimplePillTheme
+import com.tazy.simplepillfinal.ui.theme.SimplePillTheme // O nome do seu tema
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SimplePillTheme {
-                // CORREÇÃO: Crie o NavController aqui
-                val navController = rememberNavController()
-                // E passe-o para o NavGraph
-                NavGraph(navController = navController)
+            SimplePillTheme { // Use o seu tema aqui
+                NavGraph()
             }
         }
     }
